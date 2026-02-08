384e670a0bf00f0cb22931e33f6b828d2e7faa86
R Aditya Murty <adityamurty2006@gmail.com>
2026-02-08 22:19:06 +0530

    navbar fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
-
-import Navbar from "@/components/Navbar"; // Navbar import
 import Index from "./pages/Index";
 import ExamImpact from "./pages/ExamImpact";
 import AboutUs from "./pages/AboutUs";
