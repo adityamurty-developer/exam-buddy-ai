a11b707f6cf60edccf647cb14cb205acf00547a5
R Aditya Murty <adityamurty2006@gmail.com>
2026-02-08 22:15:21 +0530

    navbar-mobile-responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toaster"; 
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "next-themes";
+
+import Navbar from "@/components/Navbar"; // Navbar import
 import Index from "./pages/Index";
 import ExamImpact from "./pages/ExamImpact";
 import AboutUs from "./pages/AboutUs";
@@ -11,18 +13,18 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
+const App: React.FC = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <Navbar /> {/* Navbar added */}
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/news" element={<ExamImpact />} />
             <Route path="/about" element={<AboutUs />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
