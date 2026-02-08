310ffbf7f211d3aa5f288acafc0636e14566550a
R Aditya Murty <adityamurty2006@gmail.com>
2026-02-08 22:23:46 +0530

    app-tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
 import { Toaster } from "@/components/ui/toaster"; 
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "next-themes";
-import Index from "./pages/Index";
-import ExamImpact from "./pages/ExamImpact";
-import AboutUs from "./pages/AboutUs";
-import NotFound from "./pages/NotFound";
+import { Toaster as Sonner } from "@/components/ui/sonner"; 
+import { TooltipProvider } from "@/components/ui/tooltip"; 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import { ThemeProvider } from "next-themes"; 
 
-const queryClient = new QueryClient();
+import Index from "./pages/Index"; 
+import ExamImpact from "./pages/ExamImpact"; 
+import AboutUs from "./pages/AboutUs"; 
+import NotFound from "./pages/NotFound"; 
 
-const App: React.FC = () => (
+const queryClient = new QueryClient(); 
+
+const App = () => (
   <QueryClientProvider client={queryClient}>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <TooltipProvider>
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <Navbar /> {/* Navbar added */}
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/news" element={<ExamImpact />} />
             <Route path="/about" element={<AboutUs />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
-);
+); 
 
 export default App;
+
